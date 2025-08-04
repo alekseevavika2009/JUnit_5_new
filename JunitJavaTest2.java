6771cde498c5fe8bf01bf113d418308a261e4c88
alekseevavika2009 <153806218+alekseevavika2009@users.noreply.github.com>
2025-08-04 11:09:03 +0300

    Update JunitJavaTest2.java

--- JunitJavaTest2.java
+++ JunitJavaTest2.java
@@ -1,4 +1,35 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
-class JunitJavaTest2 {
-  
-}
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+
+    class JunitJavaTest2 extends JunitJavaTest {
+
+        @Test
+        @DisplayName("Проверка URL")
+        void fistTest() {
+            webdriver().shouldHave(url("https://www.litres.ru/auth/login"));
+        }
+
+        @Test
+        @DisplayName("Epic fail")
+        @Disabled("Test is failing")
+        void secondTest() {
+            webdriver().shouldNotHave(url("https://www.litres.ru/auth/login"));
+        }
+
+        @Test
+        @DisplayName("Проверка кнопки Забыли пароль")
+        void thirdTest() {
+            $(byXpath("//*[@id=\"main\"]/div/div/div/div/a/strong")).click();
+            webdriver().shouldHave(url("https://platform.productstar.ru/forgot-password"));
+        }
+    }
