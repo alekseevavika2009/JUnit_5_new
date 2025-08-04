f881e1ca0135362fa02fb2289ac7f6c28da05421
alekseevavika2009 <153806218+alekseevavika2009@users.noreply.github.com>
2025-08-04 11:09:19 +0300

    Update JunitJavaTest3.java

--- JunitJavaTest3.java
+++ JunitJavaTest3.java
@@ -1,4 +1,31 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
-class JunitJavaTest3 {
-  
-}
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.webdriver;
+import static com.codeborne.selenide.WebDriverConditions.url;
+
+    @Tags({
+            @Tag("SecondFeature"), @Tag("Smoke")
+    })
+    class JunitJavaTest3 extends JunitJavaTest {
+
+        @Test
+        void fistTest() {
+            webdriver().shouldHave(url("https://www.litres.ru/auth/login"));
+        }
+
+        @Test
+        void secondTest() {
+            webdriver().shouldNotHave(url("https://www.litres.ru/auth/login"));
+        }
+
+
+    }
